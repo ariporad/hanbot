e558f2d1c0078de691e576deb7616a20d558d568
Ari Porad <ari@ariporad.com>
2020-06-04 20:52:42 -0700

    feat: update welcome message

--- src/commands/welcome.ts
+++ src/commands/welcome.ts
@@ -19,15 +19,15 @@ export function sendWelcomeMessage(
 	welcomeChannel.send(formatMessage(guild)`
 Welcome to the Olin Class of 2024 Discord${member ? `, ${member}` : ''}!
 
-This Discord serves as a place for the Olin Class of 2024 (and a few gappies in the Class of 2025) to hang out, have fun, and get to know each other.
+This Discord serves as a place for the Olin Class of 2024, a few gappies in the Class of 2025, and some current students, to hang out, have fun, and get to know each other.
 
-Feel free to talk about whatever here in ${'#general'}, post a picture of your pet in ${'#pet-pics'}, or join our book club in ${'#book-club'}.
+Feel free to talk about whatever here in ${'#general'}, post a picture of your pet in ${'#pet-pics'}, or talk about The Happenings in ${'#current-events'}.
 
 Make sure to share your Instagram/Snapchat/whatever in ${'#social-media'}.
 
-Most evenings from 10PM-2AM ET, we hang out and play games on Zoom. Find more info and the link in ${'zoom'} (and/or use the \`!zoom\` command).
+Most evenings from 10PM-2AM ET, we hang out and play games on Zoom. Find more info and the link in ${'zoom'} (and/or use the \`!zoominfo\` command). You can see if anyone is on Zoom right now with the \`!zoom\` command or by looking at my (${`@Hanbot`}'s) Discord status.
 
-Finally, please assign yourself roles (time zone, major, pronouns, etc.) by clicking on your name.
+Finally, please assign yourself roles (class, time zone, city, major, pronouns, etc.) by clicking on your name.
 
 Wondering why everyone is named Han? Well, we have a cult. Ask our glorious leader, Han, for more details and for how to join!
 `);
