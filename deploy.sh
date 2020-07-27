0565fb600b4447cfd3d171f3d905907521d7a672
Elvis Wolcott <elviswolcott@gmail.com>
2020-07-26 22:01:08 -0500

    feat: use ignore file for deploys

--- deploy.sh
+++ deploy.sh
@@ -12,31 +12,15 @@ TARGET="hanbot@hanbot.elviswolcott.com"
 # name to give job (mostly for debugging on server)
 JOB_NAME="hanbot"
 
-# --------- Create .env ---------- #
-echo "\
-DISCORD_WELCOME_CHANNEL=${DISCORD_WELCOME_CHANNEL}
-DISCORD_ADMITTED_ROLE=${DISCORD_ADMITTED_ROLE}
-DISCORD_ACTIVE_ROLE=${DISCORD_ACTIVE_ROLE}
-DISCORD_TOKEN=${DISCORD_TOKEN}
-ZOOM_API_KEY=${ZOOM_API_KEY}
-ZOOM_API_SECRET=${ZOOM_API_SECRET}
-ZOOM_WEBHOOK_SCRET=${ZOOM_WEBHOOK_SECRET}
-ZOOM_MEETING_ID=${ZOOM_MEETING_ID}
-ZOOM_TIME_THRESHOLD=${ZOOM_TIME_THRESHOLD}
-ZOOM_TIME_ANNOUNCEMENT_CHANNEL=${ZOOM_TIME_ANNOUNCEMENT_CHANNEL}
+# -------- Append to .env -------- #
+echo "
 TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
 TRAVIS_COMMIT=${TRAVIS_COMMIT}
 " >> ".env"
 
-
 # --------- Deploy Steps --------- #
-# remove extra files
-rm "${BUILD_DIR}/.tsBuildInfo"
 # transfer the build to the target
-rsync -r --delete-after --quiet "${TRAVIS_BUILD_DIR}/${BUILD_DIR}" "${TARGET}:${TEMP_DIR}"
-# bring over package.json & .env
-rsync -r --delete-after --quiet "${TRAVIS_BUILD_DIR}/package.json" "${TARGET}:${TEMP_DIR}/package.json"
-rsync -r --delete-after --quiet "${TRAVIS_BUILD_DIR}/.env" "${TARGET}:${TEMP_DIR}/.env"
+rsync -r --delete-after --quiet --exclude-from=".deployignore" "${TRAVIS_BUILD_DIR}/" "${TARGET}:${TEMP_DIR}"
 # restart pm2 on the target
 ssh "${TARGET}" 'bash -s' <<RESTART
 cd "${TEMP_DIR}"
