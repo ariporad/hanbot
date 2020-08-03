0a16bfb9863b408469e98ae92c96b567790db171
Elvis Wolcott <elviswolcott@gmail.com>
2020-08-03 01:55:17 -0500

    chore: format

--- src/config.ts
+++ src/config.ts
@@ -1,90 +1,74 @@
-import { sign } from "jsonwebtoken";
-import dotenv from "dotenv";
+import { sign } from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 // load environment variables from .env
 dotenv.config();
 
 // We have to require this because Typescript can't handle it.
-export const { version } = require("../package.json");
+export const { version } = require('../package.json');
 
 export const START_TIME = new Date();
 
 // A random number used to differentiate multiple instances running at the same time.
 export const FAVORITE_NUMBER = Math.floor(Math.random() * 100);
 
-function getEnvVar(name: string, errorMessage: string = ""): string {
-  if (typeof process.env[name] === "string" && process.env[name]) {
-    return process.env[name] as string;
-  }
-  throw new Error(`Missing Environment Variable $${name}! ${errorMessage}`);
+function getEnvVar(name: string, errorMessage: string = ''): string {
+	if (typeof process.env[name] === 'string' && process.env[name]) {
+		return process.env[name] as string;
+	}
+	throw new Error(`Missing Environment Variable $${name}! ${errorMessage}`);
 }
 
-export const ZOOM_MEETING_ID = getEnvVar(
-  "ZOOM_MEETING_ID",
-  "Need a Zoom Meeting ID to Monitor!"
-);
+export const ZOOM_MEETING_ID = getEnvVar('ZOOM_MEETING_ID', 'Need a Zoom Meeting ID to Monitor!');
 
-const ZOOM_API_KEY = getEnvVar("ZOOM_API_KEY", "Need a Zoom JWT API Key!");
+const ZOOM_API_KEY = getEnvVar('ZOOM_API_KEY', 'Need a Zoom JWT API Key!');
 
-const ZOOM_API_SECRET = getEnvVar(
-  "ZOOM_API_SECRET",
-  "Need a Zoom API Secret Token!"
-);
+const ZOOM_API_SECRET = getEnvVar('ZOOM_API_SECRET', 'Need a Zoom API Secret Token!');
 
 export const ZOOM_WEBHOOK_SECRET = process.env.ZOOM_WEBHOOK_SECRET || null;
 
 if (!ZOOM_WEBHOOK_SECRET) {
-  console.warn(
-    "WARNING: $ZOOM_WEBHOOK_SECRET is not set. Zoom webhook messages will not be verified."
-  );
+	console.warn(
+		'WARNING: $ZOOM_WEBHOOK_SECRET is not set. Zoom webhook messages will not be verified.',
+	);
 }
 
-export const ZOOM_TIME_THRESHOLD =
-  parseInt(process.env.ZOOM_TIME_THRESHOLD || "NaN", 10) || null;
+export const ZOOM_TIME_THRESHOLD = parseInt(process.env.ZOOM_TIME_THRESHOLD || 'NaN', 10) || null;
 export const ZOOM_TIME_DEBOUNCE_HOURS =
-  parseInt(process.env.ZOOM_TIME_DEBOUNCE_HOURS || "NaN", 10) || 0;
+	parseInt(process.env.ZOOM_TIME_DEBOUNCE_HOURS || 'NaN', 10) || 0;
 export const ZOOM_TIME_ANNOUNCEMENT_CHANNEL = ZOOM_TIME_THRESHOLD
-  ? getEnvVar(
-    "ZOOM_TIME_ANNOUNCEMENT_CHANNEL",
-    "Need a Zoom Time Announcement Channel if $ZOOM_TIME_THRESHOLD is set."
-  )
-  : null;
+	? getEnvVar(
+			'ZOOM_TIME_ANNOUNCEMENT_CHANNEL',
+			'Need a Zoom Time Announcement Channel if $ZOOM_TIME_THRESHOLD is set.',
+	  )
+	: null;
 
-export const DISCORD_TOKEN = getEnvVar(
-  "DISCORD_TOKEN",
-  "Need a Discord Bot User Secret!"
-);
+export const DISCORD_TOKEN = getEnvVar('DISCORD_TOKEN', 'Need a Discord Bot User Secret!');
 export const PERSISTED_STATE_FILE = getEnvVar(
-  "PERSISTED_STATE_FILE",
-  "Need a Persisted State File!"
+	'PERSISTED_STATE_FILE',
+	'Need a Persisted State File!',
 );
 export const DISCORD_ADMITTED_ROLE = process.env.DISCORD_ADMITTED_ROLE || null;
 export const DISCORD_ZOOM_ACTIVE_ROLE = process.env.DISCORD_ZOOM_ACTIVE_ROLE || null;
-export const DISCORD_WELCOME_CHANNEL =
-  process.env.DISCORD_WELCOME_CHANNEL || null;
+export const DISCORD_WELCOME_CHANNEL = process.env.DISCORD_WELCOME_CHANNEL || null;
 
-export const PRODUCTION = process.env.NODE_ENV === "production";
+export const PRODUCTION = process.env.NODE_ENV === 'production';
 
 if (!DISCORD_ADMITTED_ROLE) {
-  console.warn(
-    "WARNING: $DISCORD_ADMITTED_ROLE is not set. No role will be added to new users."
-  );
+	console.warn('WARNING: $DISCORD_ADMITTED_ROLE is not set. No role will be added to new users.');
 }
 
 if (!DISCORD_ZOOM_ACTIVE_ROLE) {
-  console.warn(
-    "WARNING: $DISCORD_ZOOM_ACTIVE_ROLE is not set. No role will be added to users on Zoom."
-  );
+	console.warn(
+		'WARNING: $DISCORD_ZOOM_ACTIVE_ROLE is not set. No role will be added to users on Zoom.',
+	);
 }
 
 if (!DISCORD_WELCOME_CHANNEL) {
-  console.warn(
-    "WARNING: $DISCORD_WELCOME_CHANNEL is not set. No welcome message will be sent for new users."
-  );
+	console.warn(
+		'WARNING: $DISCORD_WELCOME_CHANNEL is not set. No welcome message will be sent for new users.',
+	);
 }
 
 export const getZoomToken = () =>
-  sign(
-    { iss: ZOOM_API_KEY, exp: new Date().getTime() + 5000 },
-    ZOOM_API_SECRET
-  );
+	sign({ iss: ZOOM_API_KEY, exp: new Date().getTime() + 5000 }, ZOOM_API_SECRET);
--- src/zoom.ts
+++ src/zoom.ts
@@ -1,206 +1,199 @@
-import request from "request-promise";
-import { ResponseError } from "./webhooks/helpers";
+import request from 'request-promise';
+import { ResponseError } from './webhooks/helpers';
 import {
-  ZOOM_MEETING_ID,
-  getZoomToken,
-  ZOOM_TIME_THRESHOLD,
-  ZOOM_TIME_ANNOUNCEMENT_CHANNEL,
-  ZOOM_TIME_DEBOUNCE_HOURS,
-  PRODUCTION,
-} from "./config";
-import { dispatch, getState } from "./store";
+	ZOOM_MEETING_ID,
+	getZoomToken,
+	ZOOM_TIME_THRESHOLD,
+	ZOOM_TIME_ANNOUNCEMENT_CHANNEL,
+	ZOOM_TIME_DEBOUNCE_HOURS,
+	PRODUCTION,
+} from './config';
+import { dispatch, getState } from './store';
 import {
-  getOnlineUsers,
-  getIsActive,
-  userLeave,
-  userJoin,
-  callStart,
-  callEnd,
-  setJoinUrl,
-} from "./store/zoom";
+	getOnlineUsers,
+	getIsActive,
+	userLeave,
+	userJoin,
+	callStart,
+	callEnd,
+	setJoinUrl,
+} from './store/zoom';
 
 /**
  * Check the status of the Zoom call.
  */
 export async function updateZoomStatus(): Promise<void> {
-  const meetingInfo = await request.get({
-    uri: `https://api.zoom.us/v2/meetings/${ZOOM_MEETING_ID}`,
-    auth: {
-      bearer: getZoomToken(),
-    },
-    headers: {
-      "User-Agent": "Hanbot",
-      "Content-Type": "application/json",
-    },
-    json: true,
-  });
+	const meetingInfo = await request.get({
+		uri: `https://api.zoom.us/v2/meetings/${ZOOM_MEETING_ID}`,
+		auth: {
+			bearer: getZoomToken(),
+		},
+		headers: {
+			'User-Agent': 'Hanbot',
+			'Content-Type': 'application/json',
+		},
+		json: true,
+	});
 
-  const active = meetingInfo.status === "started";
-  const wasActive = getIsActive(getState());
-  // status change
-  if (wasActive !== active) {
-    if (active) {
-      dispatch(callStart());
-    } else {
-      dispatch(callEnd());
-    }
-  }
-  dispatch(setJoinUrl(meetingInfo.join_url));
+	const active = meetingInfo.status === 'started';
+	const wasActive = getIsActive(getState());
+	// status change
+	if (wasActive !== active) {
+		if (active) {
+			dispatch(callStart());
+		} else {
+			dispatch(callEnd());
+		}
+	}
+	dispatch(setJoinUrl(meetingInfo.join_url));
 }
 
-const getZoomUserIdFromEvent = (
-  event: ZoomParticipantJoinedEvent | ZoomParticipantLeftEvent
-) => {
-  /**
-   * When the user is signed in, the event includes a stable id
-   * When they are not signed in, the id is ommited
-   * Because the user_id is not stable and can be assigned to different users during different meetings (sessions),
-   * it is neccessary to prefix it with the meeting uuid
-   */
-  const participant = event.payload.object.participant;
-  return (
-    participant.id || `${event.payload.object.uuid}.${participant.user_id}`
-  );
+const getZoomUserIdFromEvent = (event: ZoomParticipantJoinedEvent | ZoomParticipantLeftEvent) => {
+	/**
+	 * When the user is signed in, the event includes a stable id
+	 * When they are not signed in, the id is ommited
+	 * Because the user_id is not stable and can be assigned to different users during different meetings (sessions),
+	 * it is neccessary to prefix it with the meeting uuid
+	 */
+	const participant = event.payload.object.participant;
+	return participant.id || `${event.payload.object.uuid}.${participant.user_id}`;
 };
 
 export async function processWebhookEvent(event: ZoomEvent) {
-  console.log(`Processing Zoom Webhook Event: ${event.event}`);
-  if (!PRODUCTION) console.log(JSON.stringify(event, null, 2));
+	console.log(`Processing Zoom Webhook Event: ${event.event}`);
+	if (!PRODUCTION) console.log(JSON.stringify(event, null, 2));
 
-  const { id: meetingId } = event.payload.object;
+	const { id: meetingId } = event.payload.object;
 
-  // We toString both IDs because they're numerical and I don't want funny type errors.
-  if (!meetingId || meetingId.toString() !== ZOOM_MEETING_ID.toString()) {
-    console.log(`Webhook is for a different meeting (${meetingId}), ignoring.`);
-    return;
-  }
+	// We toString both IDs because they're numerical and I don't want funny type errors.
+	if (!meetingId || meetingId.toString() !== ZOOM_MEETING_ID.toString()) {
+		console.log(`Webhook is for a different meeting (${meetingId}), ignoring.`);
+		return;
+	}
 
-  await updateZoomStatus();
+	await updateZoomStatus();
 
-  switch (event.event) {
-    // We don't want to trust Zoom's webhooks for synchronization reasons, so we re-fetch the status above
-    case "meeting.started":
-    case "meeting.ended": {
-      break;
-    }
-    case "meeting.participant_joined": {
-      const participant = event.payload.object.participant;
-      dispatch(
-        userJoin({
-          zoomId: getZoomUserIdFromEvent(event),
-          name: participant.user_name,
-          temporary: !participant.id,
-        })
-      );
-      break;
-    }
-    case "meeting.participant_left": {
-      dispatch(
-        userLeave({
-          zoomId: getZoomUserIdFromEvent(event),
-          name: event.payload.object.participant.user_name,
-        })
-      );
-      break;
-    }
-    default: {
-      throw new ResponseError(
-        `Unknown Webhook Event!: ${(event as ZoomEvent).event}`,
-        400
-      );
-    }
-  }
+	switch (event.event) {
+		// We don't want to trust Zoom's webhooks for synchronization reasons, so we re-fetch the status above
+		case 'meeting.started':
+		case 'meeting.ended': {
+			break;
+		}
+		case 'meeting.participant_joined': {
+			const participant = event.payload.object.participant;
+			dispatch(
+				userJoin({
+					zoomId: getZoomUserIdFromEvent(event),
+					name: participant.user_name,
+					temporary: !participant.id,
+				}),
+			);
+			break;
+		}
+		case 'meeting.participant_left': {
+			dispatch(
+				userLeave({
+					zoomId: getZoomUserIdFromEvent(event),
+					name: event.payload.object.participant.user_name,
+				}),
+			);
+			break;
+		}
+		default: {
+			throw new ResponseError(`Unknown Webhook Event!: ${(event as ZoomEvent).event}`, 400);
+		}
+	}
 }
 
 type ZoomMeetingType =
-  | /* Instant */ 1
-  | /* Scheduled */ 2
-  | /* Recurring (no fixed time) */ 3
-  | /* Recurring (fixed time) */ 8;
+	| /* Instant */ 1
+	| /* Scheduled */ 2
+	| /* Recurring (no fixed time) */ 3
+	| /* Recurring (fixed time) */ 8;
 
 type ZoomEvent =
-  | ZoomMeetingStartedEvent
-  | ZoomMeetingEndedEvent
-  | ZoomParticipantJoinedEvent
-  | ZoomParticipantLeftEvent;
+	| ZoomMeetingStartedEvent
+	| ZoomMeetingEndedEvent
+	| ZoomParticipantJoinedEvent
+	| ZoomParticipantLeftEvent;
 
 interface ZoomMeetingStartedEvent {
-  event: "meeting.started";
-  payload: {
-    account_id: string;
-    operator: string;
-    object: {
-      uuid: string;
-      id: string;
-      host_id: string;
-      topic: string;
-      type: ZoomMeetingType;
-      start_time: string;
-      duration: number;
-      timezone: string;
-    };
-  };
+	event: 'meeting.started';
+	payload: {
+		account_id: string;
+		operator: string;
+		object: {
+			uuid: string;
+			id: string;
+			host_id: string;
+			topic: string;
+			type: ZoomMeetingType;
+			start_time: string;
+			duration: number;
+			timezone: string;
+		};
+	};
 }
 
 interface ZoomMeetingEndedEvent {
-  event: "meeting.ended";
-  payload: {
-    account_id: string;
-    object: {
-      uuid: string;
-      id: string;
-      host_id: string;
-      topic: string;
-      type: ZoomMeetingType;
-      start_time: string;
-      duration: number;
-      timezone: string;
-    };
-  };
+	event: 'meeting.ended';
+	payload: {
+		account_id: string;
+		object: {
+			uuid: string;
+			id: string;
+			host_id: string;
+			topic: string;
+			type: ZoomMeetingType;
+			start_time: string;
+			duration: number;
+			timezone: string;
+		};
+	};
 }
 
 interface ZoomParticipantJoinedEvent {
-  event: "meeting.participant_joined";
-  payload: {
-    account_id: string;
-    object: {
-      uuid: string;
-      id: string;
-      host_id: string;
-      topic: string;
-      type: ZoomMeetingType;
-      start_time: string;
-      duration: number;
-      timezone: string;
-      participant: {
-        user_id: string;
-        user_name: string;
-        id: string;
-        join_time: string;
-      };
-    };
-  };
+	event: 'meeting.participant_joined';
+	payload: {
+		account_id: string;
+		object: {
+			uuid: string;
+			id: string;
+			host_id: string;
+			topic: string;
+			type: ZoomMeetingType;
+			start_time: string;
+			duration: number;
+			timezone: string;
+			participant: {
+				user_id: string;
+				user_name: string;
+				id: string;
+				join_time: string;
+			};
+		};
+	};
 }
 
 interface ZoomParticipantLeftEvent {
-  event: "meeting.participant_left";
-  payload: {
-    account_id: string;
-    object: {
-      uuid: string;
-      id: string;
-      host_id: string;
-      topic: string;
-      type: ZoomMeetingType;
-      start_time: string;
-      duration: number;
-      timezone: string;
-      participant: {
-        user_id: string;
-        user_name: string;
-        id: string;
-        leave_time: string;
-      };
-    };
-  };
+	event: 'meeting.participant_left';
+	payload: {
+		account_id: string;
+		object: {
+			uuid: string;
+			id: string;
+			host_id: string;
+			topic: string;
+			type: ZoomMeetingType;
+			start_time: string;
+			duration: number;
+			timezone: string;
+			participant: {
+				user_id: string;
+				user_name: string;
+				id: string;
+				leave_time: string;
+			};
+		};
+	};
 }
